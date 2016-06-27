368381268fb67e4e5d5431e2e02eb50e245db472
Andreas Stallinger <astallinger@gmail.com>
2016-06-27 15:51:22 +0200

    first commit :w
    * travis README update
    * travis update maybe broken
    * menu, icon
    * deploy script
    * index.html added
    * deploy dir
    * travis file fix
    * remove repoitory folder
    * readme update
    * add license.txt
    * icon

---
+++ eCoati.core/src/main/java/io/coati/eCoati/core/preferences/CoatiPreferencePage.java
@@ -0,0 +1,65 @@
+package io.coati.eCoati.core.preferences;
+
+import org.eclipse.jface.preference.*;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import io.coati.eCoati.core.Activator;
+import io.coati.eCoati.core.preferences.PreferenceConstants;
+
+import org.eclipse.ui.IWorkbench;
+
+/**
+ * This class represents a preference page that
+ * is contributed to the Preferences dialog. By 
+ * subclassing <samp>FieldEditorPreferencePage</samp>, we
+ * can use the field support built into JFace that allows
+ * us to create a page that is small and knows how to 
+ * save, restore and apply itself.
+ * <p>
+ * This page is used to modify preferences only. They
+ * are stored in the preference store that belongs to
+ * the main plug-in class. That way, preferences can
+ * be accessed directly via the preference store.
+ */
+
+public class CoatiPreferencePage
+	extends FieldEditorPreferencePage
+	implements IWorkbenchPreferencePage {
+
+	public CoatiPreferencePage() {
+		super(GRID);
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("A demonstration of a preference page implementation");
+	}
+	
+	/**
+	 * Creates the field editors. Field editors are abstractions of
+	 * the common GUI blocks needed to manipulate various types
+	 * of preferences. Each field editor knows how to save and
+	 * restore itself.
+	 */
+	public void createFieldEditors() {
+		addField(
+			new IntegerFieldEditor(
+				PreferenceConstants.P_COATI_TO_ECLIPSE_PORT, 
+				"Port(Coati->Eclipse):", 
+				getFieldEditorParent()));
+		addField(
+			new IntegerFieldEditor(
+				PreferenceConstants.P_ECLIPSE_TO_COATI_PORT, 
+				"Port(Eclipse->Coati):", 
+				getFieldEditorParent()));
+		addField(
+			new StringFieldEditor(
+				PreferenceConstants.P_IP, 
+				"Ip:", 
+				getFieldEditorParent()));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+	 */
+	public void init(IWorkbench workbench) {
+	}
+	
+}
---
+++ eCoati.core/src/main/java/io/coati/eCoati/core/preferences/PreferenceConstants.java
@@ -0,0 +1,22 @@
+package io.coati.eCoati.core.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING = "stringPreference";
+	
+	public static final String P_COATI_TO_ECLIPSE_PORT = "coatiToEclipsePort";
+
+	public static final String P_ECLIPSE_TO_COATI_PORT = "eclipseToCoatiPort";
+	
+	public static final String P_IP = "coatiIp";;
+	
+}
